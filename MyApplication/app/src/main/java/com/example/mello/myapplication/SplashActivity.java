f1b9195e5edc34efc2e0d49931c14a7d54f18a0c
DESKTOP-D3K6OQV\mello <mellow3632@naver.com>
2018-06-03 02:16:00 +0900

    1

---
+++ MyApplication/app/src/main/java/com/example/mello/myapplication/SplashActivity.java
@@ -0,0 +1,33 @@
+package com.example.mello.myapplication;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+
+                finish();
+            }
+        }, 3000);
+    }
+}
