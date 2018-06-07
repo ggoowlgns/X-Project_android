dd4e45dd3ffaf67f0546993082b03a0334c8689b
ggoowlgns <30282681+ggoowlgns@users.noreply.github.com>
2018-06-07 19:50:49 +0900

    test

--- MyApplication/app/src/main/java/com/example/mello/myapplication/Util/Constants.java
+++ MyApplication/app/src/main/java/com/example/mello/myapplication/Util/Constants.java
@@ -7,10 +7,10 @@ package com.example.mello.myapplication.Util;
 public class Constants {
   public static final String isaAddr = "http://18.179.74.220:8000/";
 
-<<<<<<< HEAD
+
 //  public static final String  isaAddr = "http://192.168.0.11:8000/";
-=======
-  public static final String  isaAddr = "http://18.179.74.220:8000/";
->>>>>>> f4f50be3c67f7d0a0476ccc869a02e9eec70e3ef
+
+
+
 
 }
